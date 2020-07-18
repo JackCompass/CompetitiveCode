91201892648831d95c50fd1e5c8fea5de2ea5bb4
JackCompass <47244602+JackCompass@users.noreply.github.com>
2020-07-18 14:12:35 +0530

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -24,6 +24,13 @@ def bit_score(num):
 total_no_input = int(input())
 
 user_num = []
+even_pos_list = []
+odd_pos_list = []
+div_value = 0
+dis_even_list = []
+dis_odd_list = []
+count = 0
+pair_count = 0
 
 # Taking input of the user.
  
@@ -33,4 +40,33 @@ for x in range(0,total_no_input):
 
 bit_score_list = list(map(bit_score,user_num))
 
-print(bit_score_list)
+for x in range(1, total_no_input + 1):
+	if x % 2 == 0:
+		div_value = int(bit_score_list[x-1] / 10)
+		even_pos_list.append(int(div_value))
+	else:
+		div_value = int(bit_score_list[x-1] / 10)
+		odd_pos_list.append(int(div_value))
+
+dis_odd_list = list(set(odd_pos_list))
+dis_even_list = list(set(even_pos_list))
+
+for x in dis_even_list:
+	for y in even_pos_list:
+		if x == y:
+			count += 1
+
+	if (count - 1) > 0:
+		pair_count += (count - 1)
+	count = 0
+
+for x in dis_odd_list:
+	for y in odd_pos_list:
+		if x == y:
+			count += 1
+
+	if (count - 1) > 0:
+		pair_count += (count - 1)
+	count = 0
+
+print(pair_count)
