27ccc014de8b83e03b4f434b58a84917d7a986d8
Jack <kumaranuj1303@gmail.com>
2020-09-19 23:23:50 +0530

    position of large groups

---
+++ twice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s = "abcdddeeeeaabbbcd";
+	int count = 0;
+	// for (auto i = 0; i < s.length(); ++i)
+	cout << s.length() << endl;
+	return 0;
+
+}
