6c3f6fc9a28b5b4081858c07290539918591dcf0
Jack <kumaranuj1303@gmail.com>
2020-09-26 15:23:21 +0530

    Features of tree with all traversals

--- tree.cpp
+++ tree.cpp
@@ -44,6 +44,36 @@ void postorder(Node *root)
 	cout << root->data << endl;
 }
 
+int treedepth(Node *root)
+{
+	if (root == NULL)
+		return 0;
+	else
+	{
+		cout << "Root value : " << root->data << endl;
+		int left_D = treedepth(root->left);
+		int right_D = treedepth(root->right);
+		cout << "Left : " << left_D << " Right : " << right_D << endl;
+		if(left_D > right_D)
+			return left_D + 1;
+		else 
+			return right_D + 1;
+	}
+}
+
+vector< vector<int>> level_vect;
+
+void levelorder(Node *root, int level)
+{
+	if (root == NULL)
+		return;
+	if (level >= level_vect.size())
+		level_vect.push_back({});
+	level_vect[level].push_back(root->data);
+	levelorder(root->left, level+1);
+	levelorder(root->right, level+1);
+}
+
 int main()
 {
 	struct Node *root = new Node(1);
@@ -54,12 +84,23 @@ int main()
 	root->right->left = new Node(6);
 	root->right->right = new Node(7);
 
-	// Post order traversal;
-	preorder(root);
-	cout << "----------------------------------------------------------------" << endl;
-	inorder(root);
-	cout << "----------------------------------------------------------------" << endl;
-	postorder(root);
+	// preorder(root);
+	// cout << "----------------------------------------------------------------" << endl;
+	// inorder(root);
+	// cout << "----------------------------------------------------------------" << endl;
+	// postorder(root);
+	// cout << < "--------------------------------------------------------------" << endl;
+	levelorder(root, 0);
 
+	// printing out the result;
+
+	for (auto i = 0; i < level_vect.size(); i++)
+	{
+		for (auto j = 0; j < level_vect[i].size(); j++)
+			cout << level_vect[i][j] << "  ";
+		cout << endl;
+	}
+	// Symmetrical function
+	// cout << treedepth(root) << endl;
 	return 0;
 }
